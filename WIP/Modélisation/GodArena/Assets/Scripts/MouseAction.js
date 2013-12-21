5db81a04842f65a5e30e0657926b3c33832a472c
MaximeHelaine <mhelainedu91@live.fr>
2013-12-21 16:10:41 +0100

    Menu

---
+++ WIP/Modélisation/GodArena/Assets/Scripts/MouseAction.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var son : AudioClip;
+var levelSuivant : String = "Quitter";
+
+function OnMouseUp() { 
+     audio.PlayOneShot(son);
+     if (levelSuivant == "Quitter")
+         Application.Quit();
+     else
+         Application.LoadLevel(levelSuivant);
+}
+
+@script RequireComponent (AudioSource)
+
+function Start () {
+
+}
+
+function Update () {
+
+}
